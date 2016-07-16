b086c243a60da6a025bfb42937845144ab92d0ea
Gourav Shah <gs@initcron.org>
2016-07-16 18:31:35 +0530

    add node.js app and dockerfile for it

---
+++ dev/server.js
@@ -0,0 +1,8 @@
+cat > server.js
+var http = require('http');
+var handleRequest = function(request, response) {
+  response.writeHead(200);
+  response.end("Hello World!");
+}
+var www = http.createServer(handleRequest);
+www.listen(8080);
